e37e67ec0b0510ed2d8850747753babeb98d69cc
Deidre-Festus <deidrefestus@hotmail.com>
2017-10-24 16:55:19 +0200

    section 1 done

---
+++ ConsoleApplication1/PersonHandler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class PersonHandler
+    {
+         public void SetAge(Person pers, int age)
+         {
+             pers.Age = age;                                 
+         }
+     
+
+        public Person CreatePerson(int age1, string fName, string lName,
+            float height, float weight)
+        {
+            Person create = new Person();
+            create.Age = age1;
+            create.FName = fName;
+            create.LName = lName;
+            create.Height = height;
+            create.Weight = weight;
+            return create;
+
+            
+            
+        }
+
+       
+/*
+        private List<Person> list = new List<Person>();
+
+        public void CreatePerson(Person create)
+        {
+            list.Add(create);
+        }
+
+        public void List()
+        {
+            foreach (var create in CreatePerson)
+            {
+                Console.WriteLine(create);
+            }
+        }
+        
+   */
+
+
+    }
+
+
+
+}
+
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,42 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            var pers = new Person();           
+
+            //    var added = true;
+
+            //    do
+            //    {
+            //        added = CreatePerson(pers);
+            //    }
+            //    while (added);
+
+            //    Console.WriteLine("=========================");
+
+            //}   
+
+            Console.WriteLine("Enter your Name: ");
+            var fName = Console.ReadLine();
+
+            Console.WriteLine("Enter your Surname: ");
+            var lName = Console.ReadLine();
+            
+            Console.WriteLine("Enter your Age: ");
+            int age1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your Height: ");
+            var height = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your Weight: ");
+            var weight = float.Parse(Console.ReadLine());
+
+            var ph = new PersonHandler();
+            ph.CreatePerson(age1, fName, lName, height, weight);
+
+            Console.WriteLine("FullName: " +fName+" "+lName+", " +"Age: "+age1+", "
+                +"Height: "+height+", " +"Weight: "+weight);
+            Console.ReadLine();
         }
     }
 }
+
